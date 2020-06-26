4eab98625f4d13f137f884d45b4138bc6d5a044d
Yan <yan.alan.he@gmail.com>
2020-06-25 21:05:58 -0400

    Updates

---
+++ precache-manifest.abfea9edee7d335c7ebcdeffd1c0cf28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7935f30ebd6409d1f6eb5c466a8829",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9135512d9a2d137d9860",
+    "url": "/monsters-rolodex/static/css/main.68d1f114.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9135512d9a2d137d9860",
+    "url": "/monsters-rolodex/static/js/main.8719c35f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
